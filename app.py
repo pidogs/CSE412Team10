3a546f5b9fc1f4d914c5a2f51c5e61c200177b54
justz <justzyen@gmail.com>
2026-04-30 15:58:52 -0700

    add an endpoint to return data from the database

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+# Justin's section don't touch unless for very minor fixes
+# Returns results of queries as JSON data.
+@app.route("/data")
+def handle_query():
+    return jsonify({})
+# End Justin's section
 
 if __name__ == "__main__":
     app.run(debug=True)
