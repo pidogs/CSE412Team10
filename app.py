b59432aced96687fdb77c35a5c944d6e5ed5a881
justz <justzyen@gmail.com>
2026-05-01 10:37:30 -0700

    support sort_col and sort_dir

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 import dbHelper
-
+from psycopg2 import sql
 app = Flask(__name__)
 
 @app.route("/")
@@ -34,7 +34,34 @@ def handle_query():
     #   description: list[str] containing column names
     #   data: list[list[<query data result type>]] containing all rows of the output
     # }
-    return jsonify({})
+    user_search = request.args.get("search")
+    user_sort_col = request.args.get("sort_col")
+    user_sort_dir = request.args.get("sort_dir")
+    
+    conn = dbHelper.getConnection()
+    cur = conn.cursor()
+
+    if user_sort_col == "" or user_sort_col == None: # Don't want sorting
+        cur.execute("""
+            SELECT * 
+            FROM "Model";
+        """)
+    else:
+        # We have f-string here but it is being used just to specify
+        # DESC or ASC (after our own processing) so it should be safe
+        # Actual user input is still being formatted using psycopg2
+        sort_dir = "DESC" if user_sort_dir == "DESC" else "ASC"
+        cur.execute(sql.SQL(f"""
+            SELECT * 
+            FROM "Model"
+            ORDER BY {{}} {sort_dir};
+        """).format(sql.Identifier(user_sort_col))) 
+    result = {
+        "description": [col.name for col in cur.description],
+        "data": cur.fetchall()
+    }
+    print(result)
+    return jsonify(result)
 # End Justin's section
 
 if __name__ == "__main__":
